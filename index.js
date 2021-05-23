ad664f567560cca5bb263ef5e0bc65300c94c196
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-05-23 14:31:37 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const os = require("os");
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
+
+exports.ping = async function (domain) {
+  var PING;
+
+  if (os.platform() == "win32")
+    PING = await exec(`ping -n 1 ${domain} | findstr time=`);
+  else PING = await exec(`ping -c 1 ${domain} | grep time=`);
+
+  var Ping = PING.stdout.trim();
+
+  var ping;
+
+  if (os.platform == "win32")
+    ping = Number(Ping.split("time=").pop().split("ms ")[0]);
+  else ping = Number(Ping.split("time=").pop().split(" ms")[0]);
+
+  return ping;
+};
