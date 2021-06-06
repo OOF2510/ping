54d8cc086c51f831981620dcfd334da9b11cf20e
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-06-06 16:56:19 -0400

    patch for windows

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 const os = require("os");
-const { promisify } = require("util");
+import { promisify } from "util";
 const exec = promisify(require("child_process").exec);
 
 exports.ping = async function (domain: String) {
   let PING;
 
   if (os.platform() == "win32")
-    PING = await exec(`ping -n 1 ${domain} | findstr time=`);
+    PING = await exec(`ping /n 1 ${domain} | findstr time=`);
   else PING = await exec(`ping -c 1 ${domain} | grep time=`);
 
   let Ping = PING.stdout.trim();
